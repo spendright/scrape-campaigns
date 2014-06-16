ab7a6385055e2b8a60b2c8c2fb5b60091276454d
David Marin <dm@davidmarin.org>
2014-06-16 12:08:11 -0700

    hammered peta scraper into a better format

--- Gemfile
+++ Gemfile
@@ -3,3 +3,4 @@ source 'https://rubygems.org'
 
 gem "json", "1.6.5"
 gem "mechanize", "2.5.1"
+gem "nokogiri", "1.5.0"
--- scrapers/peta.rb
+++ scrapers/peta.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/ruby
 
-#   Copyright 2014 thinkContext
+#   Copyright 2014 thinkContext, David Marin
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,9 +16,9 @@
 
 require 'mechanize'
 require 'json'
-require 'pp'
 
-def action_do(url,rating)
+
+def action_do(url, rating)
   a = Mechanize.new
   p = a.get(url)
   f = p.forms[1]
@@ -27,24 +27,29 @@ def action_do(url,rating)
   links = p.links_with(:href => /cruelty_free_companies_company\.aspx/)
   links.each do |l|
     cpage = l.click
-    company = Nokogiri::HTML(cpage.body)
-    name = company.css("span#ctl00_ContentPlaceHolder1_l_CompanyName").text.strip
-    website = company.css("span#ctl00_ContentPlaceHolder1_l_Website").text.strip
-    brands = company.css("span#ctl00_ContentPlaceHolder1_l_Brands").text.split(',').map {|x| x.strip}
-    com = {'name' => name, 'url' => website, 'brands' => brands, 'rating' => rating}
-    #pp com
-    $out['companies'].push(com)
+
+    body = Nokogiri::HTML(cpage.body)
+
+    company = {}
+    # TODO: handle/strip stuff in parens after company name
+    # (e.g. "Acure (Better Planet Brands)")
+    company['company'] = body.css("span#ctl00_ContentPlaceHolder1_l_CompanyName").text.strip
+    company['url'] = body.css("span#ctl00_ContentPlaceHolder1_l_Website").text.strip
+    company['brands'] = body.css("span#ctl00_ContentPlaceHolder1_l_Brands").text.split(',').map {|x| x.strip}
+
+    # url contains a lot of extraneous variables
+    rating['url'] = cpage.uri.to_s.split('&')[0]
+    rating['table'] = 'company_rating'
+    rating['company'] = company
+
+    puts rating.to_json
   end
 end
 
 safe_url = 'http://features.peta.org/cruelty-free-company-search/cruelty_free_companies_search.aspx?Donottest=8&Product=0&Dotest=-1&Regchange=-1&Country=-1&Keyword='
 avoid_url = 'http://features.peta.org/cruelty-free-company-search/cruelty_free_companies_search.aspx?Donottest=-1&Product=0&Dotest=8&Regchange=-1&Country=-1&Keyword='
 
-$out = {}
-$out['campaign'] = {'goal' => 'Inform on which companies do and do not test their products on animals.', 'campaign' => 'Beauty Without Bunnies', 'author' => 'People For the Ethical Treatmen of Animals', 'url' => 'http://features.peta.org/cruelty-free-company-search/index.aspx', 'donate_url' => 'https://secure.peta.org/site/Donation2'}
-$out['companies'] = []
+puts ({'table' => 'campaign', 'goal' => 'Stop animal testing', 'campaign' => 'Beauty Without Bunnies', 'author' => 'People For the Ethical Treatment of Animals', 'url' => 'http://features.peta.org/cruelty-free-company-search/index.aspx', 'donate_url' => 'https://secure.peta.org/site/Donation2'}).to_json
 
-action_do(safe_url,'Does not test on animals')
-action_do(avoid_url,'Tests on animals')
-
-puts $out.to_json
+action_do(safe_url, {'description' => 'Does not test on animals', 'judgment' => 1})
+action_do(avoid_url, {'description' => 'Tests on animals', 'judgment' => -1})
