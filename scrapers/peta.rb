adf224f4fe6dcc3d4f01a479969b2efb6db37c0a
David Marin <dm@davidmarin.org>
2014-06-16 11:36:33 -0700

    add Apache copyright notice to code

--- scrapers/peta.rb
+++ scrapers/peta.rb
@@ -1,5 +1,19 @@
 #!/usr/bin/ruby
 
+#   Copyright 2014 thinkContext
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 require 'mechanize'
 require 'json'
 require 'pp'
@@ -12,11 +26,11 @@ def action_do(url,rating)
   p = f.submit
   links = p.links_with(:href => /cruelty_free_companies_company\.aspx/)
   links.each do |l|
-    cpage = l.click  
+    cpage = l.click
     company = Nokogiri::HTML(cpage.body)
     name = company.css("span#ctl00_ContentPlaceHolder1_l_CompanyName").text.strip
     website = company.css("span#ctl00_ContentPlaceHolder1_l_Website").text.strip
-    brands = company.css("span#ctl00_ContentPlaceHolder1_l_Brands").text.split(',').map {|x| x.strip}    
+    brands = company.css("span#ctl00_ContentPlaceHolder1_l_Brands").text.split(',').map {|x| x.strip}
     com = {'name' => name, 'url' => website, 'brands' => brands, 'rating' => rating}
     #pp com
     $out['companies'].push(com)
