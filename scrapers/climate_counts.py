1542f82841c4f61b0afc2f73845e2afa34809224
David Marin <dm@davidmarin.org>
2014-10-14 12:37:03 -0700

    new, working climate_counts scraper

--- scrapers/climate_counts.py
+++ scrapers/climate_counts.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-#   Copyright 2014 David Marin
+#   Copyright 2014 Spendright, Inc.
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -13,22 +13,15 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-import json
 import logging
+import re
+from collections import defaultdict
+from urlparse import urljoin
 
-from srs.scrape import scrape
+from unidecode import unidecode
 
-
-# fetch everything, through the API
-# TODO: scrape http://climatecounts.org/searchresults.php?p=brands instead
-# and http://climatecounts.org/searchresults.php?p=cat (for companies
-# with no brands listed)
-# TODO: can also add a ranking of companies by sector, though we have
-# to figure out how to handle Siemens
-API_URL = ('http://api.climatecounts.org/1/Companies.json?IncludeBrands=true&'
-           'IncludeScores=true')
-
-DETAILS_URL_PATTERN = 'http://climatecounts.org/scorecard_score.php?co={:d}'
+from srs.norm import WHITESPACE_RE
+from srs.scrape import scrape_soup
 
 
 # TODO: scrape this from the page
@@ -47,25 +40,45 @@ CAMPAIGN = {
     'facebook_url': 'http://www.facebook.com/pages/Climate-Counts/7698023321',
 }
 
-SECTOR_CORRECTIONS = {
-    'Househould Products': 'Household Products',
-}
+PRODUCT_TYPES_URL = (
+    'http://www.climatecounts.org/searchresults.php?p=product_types')
 
-# brand is listed as company and vice versa
-SWAPPED_BRANDS = {
-    'Kenmore',
-}
+BRANDS_URL = 'http://www.climatecounts.org/searchresults.php?p=brands'
 
-PROGRESS_TO_JUDGMENT = {
+SECTORS_URL = 'http://www.climatecounts.org/scorecard_overview.php'
+
+DESCRIPTION_TO_JUDGMENT = {
     'Soaring': 1,
     'Striding': 1,
     'Starting': 0,
     'Stuck': -1,
 }
 
-
-
 MAX_SCORE = 100
+
+STATUS_PATH_RE = re.compile(r'.*score_(.*)\.gif$')
+
+BRAND_CORRECTIONS = {  # hilarious
+    'Climfast': 'Slimfast',
+    'Gatoraide': 'Gatorade',
+    'Litpon': 'Lipton',
+    'Mountain Des': 'Mountain Dew',
+    'Wgeaties': 'Wheaties',
+    'Talko Bell': 'Taco Bell',
+    'Siemans': 'Siemens',
+}
+
+
+IGNORE_TWITTER_HANDLES = {
+    '@BritishAirways',  # actually @British_Airways
+    '@ShakleeUpdates',  # now ShakleeHQ
+    '@UPS_News',  # ignore in favor of @UPS
+    '@theUPSstore_PR',  # ignore in favor of @UPS
+}
+
+
+# TODO: fix "Beverages-Beer"
+
 
 log = logging.getLogger(__name__)
 
@@ -73,51 +86,201 @@ log = logging.getLogger(__name__)
 def scrape_campaign():
     yield 'campaign', CAMPAIGN
 
-    log.info('Fetching all data from API')
-    j = json.loads(scrape(API_URL))
-    companies = j['Companies']
+    # keep track of brands we've seen so far
+    known_brands = defaultdict(set)
 
-    for c in companies:
-        rating = {}  # company data
+    for table, row in scrape_product_types():
+        if table == 'brand':
+            known_brands[row['company']].add(row['brand'])
+        yield table, row
 
-        name = c['Name']
-        # don't care about e.g. "(formerly Sara Lee)"
-        if ' (' in name:
-                name = name[:name.index(' (')]
+    for table, row in scrape_brands(known_brands):
+        if table == 'brand':
+            known_brands[row['company']].add(row['brand'])
+        yield table, row
 
-        if name in SWAPPED_BRANDS:
-            rating['company'] = c['Brands'][0]
-            rating['brands'] = [name]
+    for table, row in scrape_sectors(known_brands):
+        yield table, row
+
+
+def scrape_product_types():
+    log.info('scraping product types')
+    soup = scrape_soup(PRODUCT_TYPES_URL)
+
+    for a in soup.select('#search_results_results a'):
+        cat = a.text
+        cat_url = urljoin(PRODUCT_TYPES_URL, a['href'])
+
+        log.info(u'scraping category: {}'.format(cat))
+        cat_soup = scrape_soup(cat_url)
+
+        for company, brand, sector in scrape_brand_results(cat_soup):
+            yield 'category', dict(category=cat, parent_category=sector)
+            yield 'brand', dict(company=company, brand=brand, category=cat)
+
+
+def scrape_brand_results(soup):
+    """Parse brand search results into tuples of (company, brand, sector)"""
+    for p in soup.select('#search_results_results p'):
+        company = strip_company(p.a.text)
+        brand = p.text.split(' - ')[0]
+        brand = BRAND_CORRECTIONS.get(brand, brand)
+        sector = p.i.text
+
+        yield (company, brand, sector)
+
+
+def scrape_brands(known_brands):
+    log.info('scraping brands')
+    soup = scrape_soup(BRANDS_URL)
+
+    for company, brand, sector in scrape_brand_results(soup):
+        # we already have a more specific category for this brand
+        if brand in known_brands[company]:
+            continue
+
+        if sector:
+            yield 'brand', dict(company=company, brand=brand, category=sector)
         else:
-            rating['company'] = name
-            if c['Brands']:
-                rating['brands'] = c['Brands']
+            yield 'brand', dict(company=company, brand=brand)
 
-        # sadly, the IDs on the website don't match the IDs for the API
-        # TODO: scrape the website?
-        # rating['url'] = DETAILS_URL_PATTERN.format(c['CompanyID'])
 
-        sector = SECTOR_CORRECTIONS.get(c['Sector']) or c['Sector']
+def strip_company(company):
+    """strip "(i2 company)" and whitespace from company names"""
+    if ' (' in company:
+        company = company[:company.index(' (')]
+    company = company.strip()
+    if company.endswith('*'):
+        company = company[:-1]
+    return company
 
-        if '--' in sector:  # Beverages--Beer
-            cat_hierarchy = sector.split('--')
-            category = cat_hierarchy[-1]
-            for i in xrange(len(cat_hierarchy) - 1):
-                yield 'category', dict(parent_category=cat_hierarchy[i],
-                                       category=cat_hierarchy[i + 1])
+
+def scrape_sectors(known_brands):
+    log.info('scraping all sectors')
+    soup = scrape_soup(SECTORS_URL)
+
+    for a in soup.select('#sector a'):
+        log.info(u'scraping sector: {}'.format(a.text.strip()))
+        sector_url = urljoin(SECTORS_URL, a['href'])
+        sector_soup = scrape_soup(sector_url)
+
+        urls_seen = set()  # somehow getting same URLs twice
+        for a in sector_soup.select('#sector div a'):
+            # ignore http://i2.climatecounts.org links
+            if not a['href'].startswith('/'):
+                continue
+
+            if a['href'] in urls_seen:
+                continue
+
+            urls_seen.add(a['href'])
+
+            log.info(u'scraping company: {}'.format(strip_company(a.text)))
+            company_url = urljoin(sector_url, a['href'])
+
+            for record in scrape_company(company_url, known_brands):
+                yield record
+
+
+def scrape_company(url, known_brands):
+    soup = scrape_soup(url)
+
+    company = strip_company(
+            soup.select('#company_score_company_title h1')[0].text)
+
+    c = dict(company=company)
+
+    # rating
+    score = int(soup.select('#company_score_score')[0].text.strip())
+    r = dict(company=company, score=score, max_score=MAX_SCORE)
+    status_path = soup.select('#company_score_status img')[0]['src']
+    r['description'], r['judgment'] = scrape_description_and_judgment(
+        status_path)
+
+    # icon
+    icon_as = soup.select('#company_score_company_icon a')
+    if icon_as:
+        icon_a = icon_as[0]
+        c['url'] = icon_a['href']
+        c['logo_url'] = urljoin(url, icon_a.img['src'])
+
+    # sector
+    for a in soup.select('#breadcrumbs a'):
+        if 'sectors' in a['href']:
+            c['category'] = a.text
+            break  # ignore "Industry Innovators" category
+
+    # match up brands to logos
+    brands = sorted(known_brands[company])
+    sb2b = dict((smunch(b), b) for b in brands)
+
+    for img in soup.select('span.brand_icon img'):
+        logo_url = urljoin(url, img['src'])
+        sb = smunch(img['alt'])
+        sb = SMUNCHED_BRAND_CORRECTIONS.get(sb, sb)
+        if sb in sb2b:
+            brand = sb2b[sb]
+            yield 'brand', dict(
+                company=company, brand=brand, logo_url=logo_url)
         else:
-            category = sector
+            log.warn(u'No matching brand for {} ({}: {})'.format(
+                repr(img['alt']), company, u', '.join(brands)))
 
-        rating['category'] = category
+    # match twitter handles to company/brand
+    sc = smunch(company)
+    sbs = sorted(sb2b)
+    sb2th = {}
 
-        if c.get('Scores') and c['Scores'].get('Scores'):
-            scores = c['Scores']['Scores'][-1]
+    twitter_handles = [
+        a.text.strip() for a in
+        soup.select('#company_score_action_right a')]
 
-            rating['score'] = scores['Total']
-            rating['max_score'] = MAX_SCORE
+    def match_twitter_handle(th):
+        if th in IGNORE_TWITTER_HANDLES:
+            return
 
-            rating['description'] = scores['Progress']
-            rating['judgment'] = PROGRESS_TO_JUDGMENT[scores['Progress']]
-            rating['date'] = str(scores['Year'])
+        sth = smunch(th[1:])
 
-        yield 'company_rating', rating
+        for i in range(len(sth), 1, -1):
+            if (not th.endswith('Brand') and sth[:i] == sc[:i] and
+                'twitter_handle' not in c):
+                c['twitter_handle'] = th
+                return
+
+            for sb in sbs:
+                if sth[:i] == sb[:i] and sb not in sb2th:
+                    sb2th[sb] = th
+                    return
+
+        else:
+            if 'twitter_handle' not in c:
+                c['twitter_handle'] = th
+            else:
+                log.warn(u'No matching brand/company for {} ({}: {})'.format(
+                    repr(th), company, u', '.join(brands)))
+
+    for th in twitter_handles:
+        match_twitter_handle(th)
+
+    for sb, th in sb2th.iteritems():
+        brand = sb2b[sb]
+        yield 'brand', dict(company=company, brand=brand, twitter_handle=th)
+
+    yield 'company', c
+    yield 'company_rating', r
+
+
+def scrape_description_and_judgment(status_path):
+    desc = STATUS_PATH_RE.match(status_path).group(1)
+    desc = desc[0].upper() + desc[1:]  # capitalize first letter
+    return desc, DESCRIPTION_TO_JUDGMENT[desc]
+
+
+def smunch(s):
+    """Normalize s and remove whitespace."""
+    return WHITESPACE_RE.sub('', unidecode(s).lower())
+
+
+SMUNCHED_BRAND_CORRECTIONS = dict(
+    (smunch(bad), smunch(good))
+    for bad, good in BRAND_CORRECTIONS.iteritems())
